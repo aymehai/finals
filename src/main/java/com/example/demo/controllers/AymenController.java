a57d8eff49f4ac81e5003a8b285bb9aca67b25f4
Aymen <aymen@Aymens-MacBook-Pro.local>
2017-07-19 17:45:28 -0400

    done

--- src/main/java/com/example/demo/controllers/AymenController.java
+++ src/main/java/com/example/demo/controllers/AymenController.java
@@ -18,6 +18,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+//help
 
 @Controller
 public class AymenController {
